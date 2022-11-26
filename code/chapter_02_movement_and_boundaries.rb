6d14f31707d81e4151c3f06f63b960c7bcd3d1bd
brettchalupa <brettchalupa@users.noreply.github.com>
2022-11-26 01:42:18 +0000

    deploy: c59e20c87b872d49618c72dce24e778a100d3c8c

---
+++ code/chapter_02_movement_and_boundaries.rb
@@ -0,0 +1,37 @@
+def tick args
+  args.state.player_x ||= 120
+  args.state.player_y ||= 280
+  speed = 12
+  player_w = 100
+  player_h = 80
+
+  if args.inputs.left
+    args.state.player_x -= speed
+  elsif args.inputs.right
+    args.state.player_x += speed
+  end
+
+  if args.inputs.up
+    args.state.player_y += speed
+  elsif args.inputs.down
+    args.state.player_y -= speed
+  end
+
+  if args.state.player_x +  player_w > args.grid.w
+    args.state.player_x = args.grid.w - player_w
+  end
+
+  if args.state.player_x < 0
+    args.state.player_x = 0
+  end
+
+  if args.state.player_y + player_h > args.grid.h
+    args.state.player_y = args.grid.h - player_h
+  end
+
+  if args.state.player_y < 0
+    args.state.player_y = 0
+  end
+
+  args.outputs.sprites << [args.state.player_x, args.state.player_y, player_w, player_h, 'sprites/misc/dragon-0.png']
+end
