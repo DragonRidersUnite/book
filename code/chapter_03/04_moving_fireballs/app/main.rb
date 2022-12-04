a4b06946df94b6ae5fae603df4fb840c31f76c49
brettchalupa <brettchalupa@users.noreply.github.com>
2022-12-04 13:43:35 +0000

    deploy: 268f24d04df844ff746679351b8ea5ce0db04564

---
+++ code/chapter_03/04_moving_fireballs/app/main.rb
@@ -0,0 +1,51 @@
+def tick args
+  args.state.player_x ||= 120
+  args.state.player_y ||= 280
+  args.state.fireballs ||= []
+
+  speed = 12
+  player_w = 100
+  player_h = 80
+
+  if args.inputs.left
+    args.state.player_x -= speed
+  elsif args.inputs.right
+    args.state.player_x += speed
+  end
+
+  if args.inputs.up
+    args.state.player_y += speed
+  elsif args.inputs.down
+    args.state.player_y -= speed
+  end
+
+  if args.state.player_x +  player_w > args.grid.w
+    args.state.player_x = args.grid.w - player_w
+  end
+
+  if args.state.player_x < 0
+    args.state.player_x = 0
+  end
+
+  if args.state.player_y + player_h > args.grid.h
+    args.state.player_y = args.grid.h - player_h
+  end
+
+  if args.state.player_y < 0
+    args.state.player_y = 0
+  end
+
+  if args.inputs.keyboard.key_down.z ||
+      args.inputs.keyboard.key_down.j ||
+      args.inputs.controller_one.key_down.a
+    args.state.fireballs << [args.state.player_x, args.state.player_y, 'fireball']
+  end
+
+  args.state.fireballs.each do |fireball|
+    fireball[0] += speed + 2
+  end
+
+  args.outputs.labels  << args.state.fireballs
+
+  args.outputs.sprites << [args.state.player_x, args.state.player_y, player_w, player_h, 'sprites/misc/dragon-0.png']
+end
---
+++ code/chapter_03/05_switching_to_hashes/app/main.rb
@@ -0,0 +1,56 @@
+def tick args
+  args.state.player ||= {
+    x: 120,
+    y: 280,
+    w: 100,
+    h: 80,
+    speed: 12,
+    path: 'sprites/misc/dragon-0.png',
+  }
+  args.state.fireballs ||= []
+
+  if args.inputs.left
+    args.state.player.x -= args.state.player.speed
+  elsif args.inputs.right
+    args.state.player.x += args.state.player.speed
+  end
+
+  if args.inputs.up
+    args.state.player.y += args.state.player.speed
+  elsif args.inputs.down
+    args.state.player.y -= args.state.player.speed
+  end
+
+  if args.state.player.x +  args.state.player.w > args.grid.w
+    args.state.player.x = args.grid.w - args.state.player.w
+  end
+
+  if args.state.player.x < 0
+    args.state.player.x = 0
+  end
+
+  if args.state.player.y + args.state.player.h > args.grid.h
+    args.state.player.y = args.grid.h - args.state.player.h
+  end
+
+  if args.state.player.y < 0
+    args.state.player.y = 0
+  end
+
+  if args.inputs.keyboard.key_down.z ||
+      args.inputs.keyboard.key_down.j ||
+      args.inputs.controller_one.key_down.a
+    args.state.fireballs << {
+      x: args.state.player.x,
+      y: args.state.player.y,
+      text: 'fireball',
+    }
+  end
+
+  args.state.fireballs.each do |fireball|
+    fireball.x += args.state.player.speed + 2
+  end
+
+  args.outputs.labels  << args.state.fireballs
+  args.outputs.sprites << args.state.player
+end
