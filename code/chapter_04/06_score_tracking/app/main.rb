30ad9b4a0ca38052f9ca535458853342761d0785
brettchalupa <brettchalupa@users.noreply.github.com>
2022-12-06 11:57:57 +0000

    deploy: 285fdcc19a1dffd34c120a020d0630c7bef0862b

---
+++ code/chapter_04/06_score_tracking/app/main.rb
@@ -0,0 +1,89 @@
+def spawn_target(args)
+  size = 64
+  {
+    x: rand(args.grid.w * 0.4) + args.grid.w * 0.6,
+    y: rand(args.grid.h - size * 2) + size,
+    w: size,
+    h: size,
+    path: 'sprites/target.png',
+  }
+end
+
+def tick args
+  args.state.player ||= {
+    x: 120,
+    y: 280,
+    w: 100,
+    h: 80,
+    speed: 12,
+    path: 'sprites/misc/dragon-0.png',
+  }
+  args.state.fireballs ||= []
+  args.state.targets ||= [
+    spawn_target(args), spawn_target(args), spawn_target(args)
+  ]
+  args.state.score ||= 0
+
+  if args.inputs.left
+    args.state.player.x -= args.state.player.speed
+  elsif args.inputs.right
+    args.state.player.x += args.state.player.speed
+  end
+
+  if args.inputs.up
+    args.state.player.y += args.state.player.speed
+  elsif args.inputs.down
+    args.state.player.y -= args.state.player.speed
+  end
+
+  if args.state.player.x +  args.state.player.w > args.grid.w
+    args.state.player.x = args.grid.w - args.state.player.w
+  end
+
+  if args.state.player.x < 0
+    args.state.player.x = 0
+  end
+
+  if args.state.player.y + args.state.player.h > args.grid.h
+    args.state.player.y = args.grid.h - args.state.player.h
+  end
+
+  if args.state.player.y < 0
+    args.state.player.y = 0
+  end
+
+  if args.inputs.keyboard.key_down.z ||
+      args.inputs.keyboard.key_down.j ||
+      args.inputs.controller_one.key_down.a
+    args.state.fireballs << {
+      x: args.state.player.x + args.state.player.w - 12,
+      y: args.state.player.y + 10,
+      w: 32,
+      h: 32,
+      path: 'sprites/fireball.png',
+    }
+  end
+
+  args.state.fireballs.each do |fireball|
+    fireball.x += args.state.player.speed + 2
+
+    args.state.targets.each do |target|
+      if args.geometry.intersect_rect?(target, fireball)
+        args.state.targets.delete(target)
+        args.state.fireballs.delete(fireball)
+        args.state.score += 1
+        args.state.targets << spawn_target(args)
+      end
+    end
+  end
+
+  args.outputs.sprites << [args.state.player, args.state.fireballs, args.state.targets]
+  args.outputs.labels << {
+    x: 40,
+    y: args.grid.h - 40,
+    text: "Score: #{args.state.score}",
+    size_enum: 4
+  }
+end
+
+$gtk.reset
