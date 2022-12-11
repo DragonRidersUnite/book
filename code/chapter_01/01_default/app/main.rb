5c73337d60bf182fd3470fa780f44d496f2bbc2e
brettchalupa <brettchalupa@users.noreply.github.com>
2022-12-11 22:42:49 +0000

    deploy: 12e38a8716ff611dc62df4a138f53db88d4e178f

---
+++ code/chapter_01/01_default/app/main.rb
@@ -0,0 +1,6 @@
+def tick args
+  args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
+  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
+  args.outputs.labels  << [640, 420, 'Join the Discord! https://discord.dragonruby.org', 5, 1]
+  args.outputs.sprites << [576, 280, 128, 101, 'dragonruby.png']
+end
