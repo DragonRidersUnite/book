4118f998707ad687f146ea249283abee27945154
Petros Amoiridis <petros@amignosis.com>
2023-05-16 18:04:30 +0300

    Adjust Chapter 01 to the current DRGTK version

--- src/code/chapter_01/01_default/app/main.rb
+++ src/code/chapter_01/01_default/app/main.rb
@@ -1,6 +1,18 @@
 def tick args
-  args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
-  args.outputs.labels  << [640, 420, 'Join the Discord! https://discord.dragonruby.org', 5, 1]
-  args.outputs.sprites << [576, 280, 128, 101, 'dragonruby.png']
+  args.outputs.labels  << [640, 540, 'Hello World!', 5, 1]
+  args.outputs.labels  << [640, 500, 'Docs located at ./docs/docs.html and 100+ samples located under ./samples', 5, 1]
+  args.outputs.labels  << [640, 460, 'Join the Discord server! https://discord.dragonruby.org', 5, 1]
+
+  args.outputs.sprites << { x: 576,
+                            y: 280,
+                            w: 128,
+                            h: 101,
+                            path: 'dragonruby.png',
+                            angle: args.state.tick_count }
+
+  args.outputs.labels  << { x: 640,
+                            y: 60,
+                            text: './mygame/app/main.rb',
+                            size_enum: 5,
+                            alignment_enum: 1 }
 end
